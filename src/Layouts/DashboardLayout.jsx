2822467c99265bbefe43aeb78d36b6515e74cb0c
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-09-01 01:31:41 +0600

    update dashboard layout

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -3,11 +3,11 @@ import logo from "/logo.png";
 import useAuth from "../Hooks/useAuth";
 import { TbLayoutDashboard } from "react-icons/tb";
 import { FiUser } from "react-icons/fi";
-// import useUserRole from "../Hooks/useUserRole";
+import useUserRole from "../Hooks/useUserRole";
 
 const DashboardLayout = () => {
   const { user } = useAuth();
-//   const { roleLoading, role } = useUserRole();
+  const { roleLoading, role } = useUserRole();
 
   // ✅ Function to close drawer on small devices
   const handleLinkClick = () => {
@@ -20,7 +20,7 @@ const DashboardLayout = () => {
 
   return (
     <div className="max-w-7xl mx-auto">
-        <div className="drawer lg:drawer-open">
+      <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 
         {/* Main Content Area */}
@@ -48,14 +48,20 @@ const DashboardLayout = () => {
                 </svg>
               </label>
             </div>
-            <div className="mx-2 font-bold px-2 flex gap-2 items-center">
+                        <Link
+              to="/"
+              className="flex items-center gap-1 mx-2"
+              onClick={handleLinkClick}
+            >
               <img
-                src={user?.photoURL}
-                alt={user?.displayName}
-                className="h-14 w-14 object-cover rounded-full"
+                src={logo}
+                alt="ShopSphere Logo"
+                className="h-8 lg:h-10 w-auto"
               />
-              <span>{user?.displayName} Dashboard</span>
-            </div>
+              <span className="text-[#392B12] font-bold text-lg">
+                ShopSphere
+              </span>
+            </Link>
           </div>
 
           {/* Page Content */}
@@ -72,7 +78,7 @@ const DashboardLayout = () => {
           ></label>
 
           {/* ✅ Keep sidebar scrollable and single-column */}
-          <ul className="menu bg-base-200 text-base-content w-56 lg:w-60 p-4 h-full overflow-y-auto flex flex-col flex-nowrap hide-scrollbar">
+          <ul className="menu bg-base-200 text-base-content pb-20 w-56 lg:w-60 p-4 h-full overflow-y-auto flex flex-col flex-nowrap hide-scrollbar">
             {/* Logo */}
             <Link
               to="/"
@@ -82,7 +88,7 @@ const DashboardLayout = () => {
               <img
                 src={logo}
                 alt="ShopSphere Logo"
-                className="h-9 lg:h-11 w-auto"
+                className="h-8 lg:h-10 w-auto"
               />
               <span className="text-[#392B12] font-bold text-lg">
                 ShopSphere
@@ -110,6 +116,24 @@ const DashboardLayout = () => {
               </NavLink>
             </li>
           </ul>
+
+          <div className="flex absolute border-t border-gray-200 bottom-0 bg-base-200 py-2 px-4 w-56 lg:w-60 gap-2 items-center">
+            <img
+              src={user?.photoURL}
+              alt={user?.displayName}
+              className="h-12 w-12 border-primary border-[1.5px] object-cover rounded-full"
+            />
+            <div>
+              <p className="font-bold text-xs">{user?.displayName}</p>
+              <p className="text-xs">
+                {roleLoading ? (
+                  <span className="animate-pulse">Loading...</span>
+                ) : (
+                  role
+                )}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
